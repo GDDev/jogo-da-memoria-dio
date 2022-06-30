84d905ecacb441354445828a6ae94e2780a27ecd
devdatovo <dev.gustavodatovo@outlook.com>
2022-06-30 11:25:45 -0300

    Adicionando um modo de reiniciar o jogo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,8 @@ const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
+let message = document.getElementById('win');
+let restart = document.getElementById('restart');
 
 function flipCard(){
     if(lockBoard) return;
@@ -16,6 +18,7 @@ function flipCard(){
     secondCard = this;
     checkForMatch();
     hasFlippedCard = false;
+    isOver();
 }
 
 function checkForMatch(){
@@ -56,6 +59,38 @@ function resetBoard(){
         card.style.order = randomPosition;
     });
 })();
+
+function isOver(){
+    let cardsFlipped = 0;
+    cards.forEach((card) => {
+        if(card.classList.contains('flip')) cardsFlipped++;
+    })
+    if(cardsFlipped === 12){
+        message.style.visibility = "visible";
+        restart.style.visibility = "visible";
+        restart.addEventListener('click', unflipAllCards);
+    }
+}
+
+function unflipAllCards(){
+    setTimeout(() => {
+        cards.forEach((card) => {
+            card.classList.remove('flip');
+        })
+        message.style.visibility = "hidden";
+        restart.style.visibility = "hidden";
+        cards.forEach((card) => {
+            card.addEventListener('click', flipCard);
+            card.style.cursor = "pointer";
+        })
+
+        resetBoard();
+        cards.forEach((card) => {
+            let randomPosition = Math.floor(Math.random() * 12);
+            card.style.order = randomPosition;
+        });
+    }, 1500);
+}
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard);
